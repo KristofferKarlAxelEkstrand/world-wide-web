9b22b1e97602ce31cb3847328252f367b8882a77
Kristoffer Ekstrand <kristoffer.ekstrand@gmail.com>
2024-11-28 18:19:18 +0100

    Lists directorries

---
+++ dir.js
@@ -0,0 +1,38 @@
+import { readdirSync, statSync, writeFileSync } from 'fs';
+import { join, resolve, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+// Function to get all directories in a given path
+function getDirectories(srcPath) {
+	return readdirSync(srcPath).filter((file) => {
+		return statSync(join(srcPath, file)).isDirectory();
+	});
+}
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const srcPath = resolve(__dirname, 'src');
+const directories = getDirectories(srcPath);
+const outputPath = resolve(__dirname, 'src/index.html');
+
+const htmlContent = `
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+		<link rel="icon" type="image/svg+xml" href="/vite.svg" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<title>World Wide Web Fiddle</title>
+	</head>
+	<body>
+		<ul>
+			${directories.map((dir) => `<li><a href="./${dir}">${dir}</a></li>`).join('')}
+		</ul>
+
+		<script type="module" src="/index.js"></script>
+	</body>
+</html>
+`;
+
+writeFileSync(outputPath, htmlContent, 'utf-8');
+
+console.log(`Folder names have been written to ${outputPath}`);
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,27 @@
 // vite.config.js
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
+import { readdirSync, statSync } from 'fs';
 
 const root = resolve(__dirname, 'src');
 const outDir = resolve(__dirname, 'dist');
+
+// Function to get all directories in a given path
+function getDirectories(srcPath) {
+	return readdirSync(srcPath).filter((file) =>
+		statSync(resolve(srcPath, file)).isDirectory()
+	);
+}
+
+// Generate input paths for each directory in 'src'
+const input = {
+	main: resolve(__dirname, 'src/index.html'),
+};
+
+getDirectories(root).forEach((dir) => {
+	const htmlFilePath = resolve(root, dir, 'index.html');
+	input[dir] = htmlFilePath;
+});
 
 export default defineConfig({
 	root,
@@ -11,13 +29,7 @@ export default defineConfig({
 		outDir,
 		emptyOutDir: true,
 		rollupOptions: {
-			input: {
-				main: resolve(__dirname, 'index.html'),
-				about: resolve(__dirname, 'about.html'),
-				css_toggle: resolve(__dirname, 'src/css_toggle/index.html'),
-				square_grid: resolve(__dirname, 'src/square_grid/index.html'),
-				products: resolve(__dirname, 'src/products/index.html'),
-			},
+			input,
 		},
 	},
 	server: {
