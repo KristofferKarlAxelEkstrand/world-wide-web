116bd2ddd86a40d5721e6f45cd2fa18067cf854e
Kristoffer Ekstrand <kristoffer.ekstrand@gmail.com>
2025-06-07 20:21:04 +0200

    refactor

--- src/midi/script.js
+++ src/midi/script.js
@@ -1,70 +1,101 @@
 import './styles.css';
 
-if (navigator.requestMIDIAccess) {
-	navigator.requestMIDIAccess().then(
-		(midiAccess) => {
-			const inputs = Array.from(midiAccess.inputs.values());
-			if (inputs.length > 0) {
-				console.log('MIDI device(s) connected');
-				inputs.forEach((input, idx) => {
-					console.log(`Device ${idx + 1}: ${input.name} [id: ${input.id}]`);
-				});
-				const input = inputs[2];
-				console.log(`Using MIDI device: ${input.name}`);
+class MidiSynth {
+	constructor() {
+		this.activeOscillators = {};
+		this.audioCtx = null;
+		this.init();
+	}
 
-				// Track active notes
-				const activeOscillators = {};
-
-				input.onmidimessage = (message) => {
-					const [status, note, velocity] = message.data;
-
-					// Note on
-					if ((status & 0xf0) === 0x90 && velocity > 0) {
-						if (!activeOscillators[note]) {
-							activeOscillators[note] = playBeep(note);
-							console.log(`Note on: ${note}`);
-						}
-					}
-					// Note off (either 0x80 or 0x90 with velocity 0)
-					if ((status & 0xf0) === 0x80 || ((status & 0xf0) === 0x90 && velocity === 0)) {
-						if (activeOscillators[note]) {
-							stopBeep(activeOscillators[note]);
-							delete activeOscillators[note];
-							console.log(`Note off: ${note}`);
-						}
-					}
-				};
-			} else {
-				console.log('No MIDI devices connected');
-			}
-		},
-		() => {
+	async init() {
+		if (!navigator.requestMIDIAccess) {
+			console.log('Web MIDI API not supported in this browser');
+			return;
+		}
+		try {
+			const midiAccess = await navigator.requestMIDIAccess();
+			this.handleMidiAccess(midiAccess);
+		} catch {
 			console.log('Could not access MIDI devices');
 		}
-	);
-} else {
-	console.log('Web MIDI API not supported in this browser');
+	}
+
+	handleMidiAccess(midiAccess) {
+		const inputs = Array.from(midiAccess.inputs.values());
+		if (inputs.length === 0) {
+			console.log('No MIDI devices connected');
+			return;
+		}
+		console.log('MIDI device(s) connected');
+		inputs.forEach((input, idx) => {
+			console.log(`Device ${idx + 1}: ${input.name} [id: ${input.id}]`);
+		});
+
+		// Use the first device by default
+		this.setInput(inputs[2]);
+	}
+
+	setInput(input) {
+		if (!input) return;
+		console.log(`Using MIDI device: ${input.name}`);
+		input.onmidimessage = this.handleMidiMessage.bind(this);
+	}
+
+	handleMidiMessage(message) {
+		const [status, note, velocity] = message.data;
+		if (this.isNoteOn(status, velocity)) {
+			this.noteOn(note);
+		} else if (this.isNoteOff(status, velocity)) {
+			this.noteOff(note);
+		}
+	}
+
+	isNoteOn(status, velocity) {
+		return (status & 0xf0) === 0x90 && velocity > 0;
+	}
+
+	isNoteOff(status, velocity) {
+		return (status & 0xf0) === 0x80 || ((status & 0xf0) === 0x90 && velocity === 0);
+	}
+
+	noteOn(note) {
+		if (!this.activeOscillators[note]) {
+			this.activeOscillators[note] = this.playBeep(note);
+			console.log(`Note on: ${note}`);
+		}
+	}
+
+	noteOff(note) {
+		if (this.activeOscillators[note]) {
+			this.stopBeep(this.activeOscillators[note]);
+			delete this.activeOscillators[note];
+			console.log(`Note off: ${note}`);
+		}
+	}
+
+	playBeep(note) {
+		if (!this.audioCtx) {
+			const AudioCtx = window.AudioContext || window.webkitAudioContext;
+			this.audioCtx = new AudioCtx();
+		}
+		const oscillator = this.audioCtx.createOscillator();
+		const gain = this.audioCtx.createGain();
+		oscillator.type = 'sawtooth';
+		gain.gain.value = 0.1;
+
+		const frequency = 440 * Math.pow(2, (note - 69) / 12);
+		oscillator.frequency.value = frequency;
+
+		oscillator.connect(gain);
+		gain.connect(this.audioCtx.destination);
+		oscillator.start();
+
+		return { oscillator, gain };
+	}
+
+	stopBeep({ oscillator }) {
+		oscillator.stop();
+	}
 }
 
-function playBeep(note) {
-	const AudioCtx = window.AudioContext || window.webkitAudioContext;
-	const ctx = new AudioCtx();
-	const oscillator = ctx.createOscillator();
-	const gain = ctx.createGain();
-	oscillator.type = 'sawtooth';
-	gain.gain.value = 0.1;
-
-	const frequency = 440 * Math.pow(2, (note - 69) / 12);
-	oscillator.frequency.value = frequency;
-
-	oscillator.connect(gain);
-	gain.connect(ctx.destination);
-	oscillator.start();
-
-	return { ctx, oscillator };
-}
-
-function stopBeep({ ctx, oscillator }) {
-	oscillator.stop();
-	ctx.close();
-}
+new MidiSynth();
