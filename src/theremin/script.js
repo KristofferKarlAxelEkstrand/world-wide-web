e6007b88a1ff796c115cca4da29c3d19721c43ed
Kristoffer Ekstrand <kristoffer.ekstrand@gmail.com>
2025-06-29 15:49:39 +0200

    add

--- src/theremin/script.js
+++ src/theremin/script.js
@@ -111,32 +111,15 @@ class ThereminApp extends HTMLElement {
 					Volume
 					<div>
 					Speed
-					<input
-						type="range"
-						min="0"
-						max="1"
-						step="0.01"
-						value="0.5"
-						style="width: 120px;"
-						aria-label="Volume"
-						oninput="this.getRootNode().host._targetGain = parseFloat(this.value)"
-					/>
+					<dial-knob min="0" max="100" value="50" step="1" sizeRem="2"></dial-knob>
+
 					</div>
 				</div>
 				<div class="theremin-settings-group">
 					Vibrato
 					<div>
 					Speed
-					<input
-						type="range"
-						min="0"
-						max="1"
-						step="0.01"
-						value="0.5"
-						style="width: 120px;"
-						aria-label="Volume"
-						oninput="this.getRootNode().host._targetGain = parseFloat(this.value)"
-					/>
+<dial-knob min="0" max="100" value="50" step="1" sizeRem="2"></dial-knob>
 					</div>
 
 										<div>
@@ -294,8 +277,6 @@ class ThereminApp extends HTMLElement {
 		this.handleMouseMove(e);
 	};
 
-	// (Removed duplicate startOscillator and stopOscillator methods)
-
 	handleMouseUp = () => {
 		this.thereminXYPad.removeEventListener('mousemove', this.handleMouseMove);
 		window.removeEventListener('mousemove', this.handleMouseMove);
@@ -303,3 +284,119 @@ class ThereminApp extends HTMLElement {
 }
 
 customElements.define('theremin-app', ThereminApp);
+
+class DialKnob extends HTMLElement {
+	static observedAttributes = ['min', 'max', 'step', 'value', 'sizeRem'];
+
+	#dragging = false;
+	#min = 0;
+	#max = 100;
+	#step = 1;
+	#value = 50;
+	#size = 32;
+	#svg = null;
+	#pointer = null;
+	#startY = 0;
+	#startValue = 0;
+
+	constructor() {
+		super();
+		this.#size = (parseFloat(this.getAttribute('sizeRem')) || 2) * 16;
+		this.innerHTML = `
+			<style>
+				dial-knob { display: inline-block; }
+				dial-knob svg { touch-action: none; user-select: none; }
+			</style>
+			<svg width="${this.#size}" height="${this.#size}" viewBox="0 0 ${this.#size} ${this.#size}">
+				<circle cx="${this.#size / 2}" cy="${this.#size / 2}" r="${this.#size / 2 - 4}" fill="#222"/>
+				<rect id="pointer"
+					x="${this.#size / 2 - (this.#size * 0.08) / 2}"
+					y="${this.#size * 0.13}"
+					width="${this.#size * 0.08}"
+					height="${this.#size * 0.32}"
+					rx="${(this.#size * 0.08) / 2}"
+					fill="#fff"/>
+			</svg>
+		`;
+		this.#svg = this.querySelector('svg');
+		this.#pointer = this.querySelector('#pointer');
+		this.#svg.addEventListener('mousedown', this.#onDown);
+		this.#svg.addEventListener('touchstart', this.#onDown, { passive: false });
+		this.#updateFromAttributes();
+		this.#draw();
+	}
+
+	connectedCallback() {
+		this.#updateFromAttributes();
+		this.#draw();
+	}
+
+	disconnectedCallback() {
+		this.#svg.removeEventListener('mousedown', this.#onDown);
+		this.#svg.removeEventListener('touchstart', this.#onDown);
+		window.removeEventListener('mousemove', this.#onMove);
+		window.removeEventListener('mouseup', this.#onUp);
+		window.removeEventListener('touchmove', this.#onMove);
+		window.removeEventListener('touchend', this.#onUp);
+	}
+
+	attributeChangedCallback() {
+		this.#updateFromAttributes();
+		this.#draw();
+	}
+
+	#updateFromAttributes() {
+		this.#min = Number(this.getAttribute('min')) ?? 0;
+		this.#max = Number(this.getAttribute('max')) ?? 100;
+		this.#step = Number(this.getAttribute('step')) ?? 1;
+		this.#value = this.hasAttribute('value') ? Number(this.getAttribute('value')) : this.#value;
+		this.#size = (parseFloat(this.getAttribute('sizeRem')) || 2) * 16;
+	}
+
+	set value(val) {
+		const v = Math.min(this.#max, Math.max(this.#min, Number(val)));
+		if (v !== this.#value) {
+			this.#value = v;
+			this.setAttribute('value', v);
+			this.#draw();
+			this.dispatchEvent(new CustomEvent('change', { detail: { value: this.#value } }));
+		}
+	}
+	get value() {
+		return this.#value;
+	}
+
+	#onDown = (e) => {
+		e.preventDefault();
+		this.#dragging = true;
+		this.#startY = (e.touches ? e.touches[0] : e).clientY;
+		this.#startValue = this.value;
+		window.addEventListener('mousemove', this.#onMove);
+		window.addEventListener('mouseup', this.#onUp);
+		window.addEventListener('touchmove', this.#onMove, { passive: false });
+		window.addEventListener('touchend', this.#onUp);
+	};
+	#onMove = (e) => {
+		if (!this.#dragging) return;
+		const y = (e.touches ? e.touches[0] : e).clientY;
+		let v = this.#startValue + ((this.#startY - y) / 150) * (this.#max - this.#min);
+		v = Math.round(v / this.#step) * this.#step;
+		v = Math.max(this.#min, Math.min(this.#max, v));
+		this.value = v;
+	};
+	#onUp = () => {
+		this.#dragging = false;
+		window.removeEventListener('mousemove', this.#onMove);
+		window.removeEventListener('mouseup', this.#onUp);
+		window.removeEventListener('touchmove', this.#onMove);
+		window.removeEventListener('touchend', this.#onUp);
+	};
+
+	#draw() {
+		const size = this.#size;
+		const t = (this.value - this.#min) / (this.#max - this.#min || 1);
+		const angle = -135 + t * 270;
+		this.#pointer.setAttribute('transform', `rotate(${angle} ${size / 2} ${size / 2})`);
+	}
+}
+customElements.define('dial-knob', DialKnob);
